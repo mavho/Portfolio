5b3fa8451a480e52cdf83e233ff618ba43b78a20
mavHo <homaverick@gmail.com>
2021-09-11 15:08:21 -0700

    Removing contact form functionality. Uneeded in the long run.

--- server/routes.py
+++ server/routes.py
@@ -1,25 +1,8 @@
 from flask import render_template, jsonify, abort, request,flash, send_file
-from server.forms import ContactForm
 from flask_mail import Message,Mail
 from server import app, mail
 import sys, os
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/',methods=['GET'])
 def index():
-    form = ContactForm(request.form)
-
-    if request.method == 'POST':
-        if form.validate() == False:
-            flash(form.errors)
-            return render_template('_layouts/base.html', form=form), 200
-        else:
-            msg = Message(form.subject.data, sender='contact@example.com', recipients=['your_email@example.com'])
-            msg.body = """
-            From: %s <%s>
-            %s
-            """ % (form.name.data, form.email.data, form.message.data)
-            mail.send(msg)
-            return render_template('_layouts/base.html', success=True,form=ContactForm(formdata=None))
-
-    elif request.method == 'GET':
-        return render_template('_layouts/base.html',form=form), 200
+    return render_template('_layouts/base.html'), 200
