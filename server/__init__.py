f050bdda12cfb472a1f4e279b8223d054956655f
Maverick <homaverick@gmail.com>
2022-03-31 03:54:43 +0000

    Remove feedback portion of website.

--- server/__init__.py
+++ server/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_mail import Mail
 from server.config import Config
 import os
 
@@ -8,7 +7,5 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 app.secret_key = app.config['SECRET_KEY']
-
-mail = Mail(app)
 
 import server.routes
--- server/routes.py
+++ server/routes.py
@@ -1,7 +1,4 @@
-from flask import render_template, jsonify, abort, request,flash, send_file
-from flask_mail import Message,Mail
-from server import app, mail
-import sys, os
+from server import app
 
 @app.route('/',methods=['GET'])
 def index():
